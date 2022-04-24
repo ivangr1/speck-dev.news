c28231846378a5bdaa3ab724448de950667ab6e1
ivan <grubi.ivan@gmail.com>
2022-04-24 03:40:14 -0700

    first commit

---
+++ app/screens/MediumDetailsNoDateScreen.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import _ from 'lodash';
+import { getRouteParams } from 'shoutem.navigation';
+import { connect } from 'react-redux';
+import { connectStyle } from '@shoutem/theme';
+import { Caption, Tile, Title, View } from '@shoutem/ui';
+import { ext } from '../const';
+import { ArticleDetailsScreen } from 'shoutem.news/screens/ArticleDetailsScreen';
+
+export class MediumDetailsNoDateScreen extends ArticleDetailsScreen {
+    renderHeader() {
+        const { article } = getRouteParams(this.props);
+        const author = _.get(article, 'newsAuthor');
+        const shouldDisplayAuthor = !_.isEmpty(author);
+
+        return (
+            <Tile styleName="text-centric md-gutter-top sm-gutter-bottom">
+                <Title>{article.title.toUpperCase()}</Title>
+                {shouldDisplayAuthor && (<View styleName="horizontal sm-gutter-top sm-gutter-bottom">
+                    <Caption numberOfLines={1}>{author}</Caption>
+                </View>)}
+            </Tile>
+        );
+    }
+}
+
+
+export default connectStyle(ext('ArticleDetailsScreen'))(MediumDetailsNoDateScreen);
